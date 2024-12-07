4b1beffa6dadb85a6d5a72ce7ca18477ddc5d7dd
Oanadp90 <oanadp90@gmail.com>
2024-12-07 18:37:00 +0000

    factorial_solved2

---
+++ factorial_solved2.py
@@ -0,0 +1,12 @@
+#Please see below a 2nd way to resolve this.
+# def factorial(n):
+#     if n == 0 or n == 1:
+#         return 1
+#     else:
+#         result = 1
+#         for i in range(2,n + 1):
+#             result = result * i
+#     return result
+# num = int(input("Enter a number: "))
+# factorial_result = factorial(num) 
+# print(f"The factorial of {num} is: {factorial_result}")
